dc6b66349df0b280fbd7cf5984bca72d71d29778
Christian Arévalo Jesús <carevalojesus@gmail.com>
2022-08-31 21:27:27 -0500

    Subiendo mi archivo  en c++

---
+++ Primer Examen/Ejemplo1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Christian  Arévalo Jesús on 31/08/22.
+//
+
+#include "Ejemplo1.h"
---
+++ Primer Examen/Ejemplo1.h
@@ -0,0 +1,14 @@
+//
+// Created by Christian  Arévalo Jesús on 31/08/22.
+//
+
+#ifndef LP4_EJEMPLO1_H
+#define LP4_EJEMPLO1_H
+
+
+class Ejemplo1 {
+
+};
+
+
+#endif //LP4_EJEMPLO1_H
